16553b5f1f781e1c9779d74f1cf416b2e66bb0a5
Rohith Achar <rohithachar2000@gmail.com>
2024-05-02 17:54:15 +0530

    express

--- server.js
+++ server.js
@@ -1,37 +1,9 @@
-// import http from "http";
-const http = require("http");
-const fs = require("fs/promises");
-// const http = require('url');
+const express = require("express");
 const path = require("path");
-// import fs from "fs/promises";
-// import url from "url";
-// import path from "path";
+const app = express();
 
-const PORT = 8080;
-console.log(__dirname);
+const PORT = process.env.PORT || 5000;
 
-//get current path
-// const __filename = url.pathToFileURL(import.meta.url);
-// const __dirname = path.dirname(__filename.pathname).split(":")[1];
+app.use(express.static(path.join(__dirname, "public")));
 
-const server = http.createServer(async (req, res) => {
-  let filePath;
-  let status = 200;
-
-  if (req.url === "/") {
-    filePath = path.join(__dirname, "index.html");
-  } else if (req.url === "/about") {
-    filePath = path.join(__dirname, "about.html");
-  } else if (req.url === "/contact-me") {
-    filePath = path.join(__dirname, "contact-me.html");
-  } else {
-    filePath = path.join(__dirname, "404.html");
-    status = 404;
-  }
-  const data = await fs.readFile(filePath);
-  res.writeHead(status, { "Content-Type": "text/html" });
-  res.write(data);
-  res.end();
-});
-
-server.listen(PORT, () => console.log(`Running on ${PORT}`));
+app.listen(PORT, console.log(`Running on localhost ${PORT}`));
